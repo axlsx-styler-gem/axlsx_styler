490317232043aa348c33ff606e71e81f701c5790
Anton Sakovich <sakovias@gmail.com>
2015-02-14 11:24:05 -0500

    Mixed in styler into Array

--- lib/axlsx_styler.rb
+++ lib/axlsx_styler.rb
@@ -3,3 +3,5 @@ require 'axlsx'
 require 'axlsx_styler/version'
 require 'axlsx_styler/array'
 require 'axlsx_styler/axlsx_extensions'
+
+Array.include AxlsxStyler::Array
--- test/array_test.rb
+++ test/array_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ArrayTest < MiniTest::Test
   def test_presesence_of_add_style
-    array = AxlsxStyler::Array.new
+    array = Array.new
     assert array.respond_to?(:add_style)
   end
 end
