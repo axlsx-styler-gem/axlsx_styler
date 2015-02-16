1237a5117af253d93760a0a2b48ef877233ae018
Anton Sakovich <sakovias@gmail.com>
2015-02-16 00:33:59 -0500

    Improved an example code file. Removed generated Excel file

--- examples/colors_and_borders.rb
+++ examples/colors_and_borders.rb
@@ -10,10 +10,14 @@ workbook.add_worksheet do |sheet|
   sheet.add_row ["", "Bread", "Baked Goods", 3.45]
   sheet.add_row ["", "Broccoli", "Produce", 2.99]
 
-  sheet["B2:D2"].add_style(b: true)
-  sheet["B2:D5"].add_style(bg_color: "E2D3EB")
+  sheet["B2:D2"].add_style b: true
+  sheet["B2:B5"].add_style b: true
+  sheet["B2:D2"].add_style bg_color: "95AFBA"
+  sheet["B3:D5"].add_style bg_color: "E2F89C"
+  sheet["D3:D5"].add_style alignment: { horizontal: :left }
   sheet["B2:D5"].add_border
-  sheet["B3:D3"].add_border([:top])
+  sheet["B3:D3"].add_border [:top]
+  sheet.column_widths 5, 20, 20, 20
 end
 
 workbook.apply_styles
