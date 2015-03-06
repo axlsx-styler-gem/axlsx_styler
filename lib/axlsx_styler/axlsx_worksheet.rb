9be3302acd5857b230b64af68cf91b70f0f7f587
Anton Sakovich <sakovias@gmail.com>
2015-03-06 01:39:55 -0500

    Extracted border drawing functionality into a separate class

--- lib/axlsx_styler/axlsx_worksheet.rb
+++ lib/axlsx_styler/axlsx_worksheet.rb
@@ -1,3 +1,5 @@
+require_relative './border_creator'
+
 module AxlsxStyler
   module Axlsx
     module Worksheet
@@ -13,50 +15,10 @@ module AxlsxStyler
       #   add_border 'B2:F8', [:left, :top]
       #   add_border 'C2:G10', [:top]
       #   add_border 'C2:G10'
+      # @TODO: allow to pass in custom border style
       def add_border(cell_ref, edges = :all)
-        selected_edges(edges).each { |edge| add_border_at(cell_ref, edge) }
-      end
-
-      private
-
-      def selected_edges(edges)
-        all_edges = [:top, :right, :bottom, :left]
-        if edges == :all
-          all_edges
-        elsif edges.is_a?(Array) && edges - all_edges == []
-          edges.uniq
-        else
-          []
-        end
-      end
-
-      # @TODO: allow for custom border width
-      def add_border_at(cell_ref, position)
-        style = {
-          border: {
-            style: :thin, color: '000000', edges: [position.to_sym]
-          }
-        }
-        add_style border_cells(cell_ref)[position.to_sym], style
-      end
-
-      def border_cells(cell_ref)
         cells = self[cell_ref]
-        first = cells.first.r
-        last  = cells.last.r
-
-        first_row = first.scan(/\d+/).first
-        last_row  = last.scan(/\d+/).first
-        first_col = first.scan(/\D+/).first
-        last_col  = last.scan(/\D+/).first
-
-        # example range "B2:D5"
-        {
-          top:     "#{first}:#{last_col}#{first_row}", # "B2:D2"
-          right:   "#{last_col}#{first_row}:#{last}",  # "D2:D5"
-          bottom:  "#{first_col}#{last_row}:#{last}",  # "B5:D5"
-          left:    "#{first}:#{first_col}#{last_row}"  # "B2:B5"
-        }
+        BorderCreator.new(self, cells, edges).draw
       end
     end
   end
