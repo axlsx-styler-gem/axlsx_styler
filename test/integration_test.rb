09447d373e3162fa5c194e69ff646ab80a5dac0d
Weston Ganger <westonganger@gmail.com>
2016-09-12 17:36:59 -0700

    fix tests

--- test/integration_test.rb
+++ test/integration_test.rb
@@ -242,10 +242,12 @@ class IntegrationTest < MiniTest::Test
 
   def test_merge_styles_1
     filename = 'merge_styles_1'
+    bold = @workbook.styles.add_style b: true
+
     @workbook.add_worksheet do |sheet|
       sheet.add_row
-      sheet.add_row ['', '1', '2', '3']
-      sheet.add_row ['', '4', '5', '6']
+      sheet.add_row ['', '1', '2', '3'], style: [nil, bold]
+      sheet.add_row ['', '4', '5', '6'], style: bold
       sheet.add_row ['', '7', '8', '9']
       sheet.add_style 'B2:D4', b: true
       sheet.add_border 'B2:D4', { style: :thin, color: '000000' }
